f8728e46f2bbb4cba210eb111d07243388798abc
ghaderi <mghadery@gmail.com>
2025-03-28 20:45:56 +0100

    C++ Hello world with a template, constexpr, auto and random numbers flavor.

---
+++ CppCrashCourse/Part01Core/Part01Core/Part01Core.h
@@ -0,0 +1,9 @@
+﻿// Part01Core.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <cstdio>
+#include <random>
+
+// TODO: Reference additional headers your program requires here.
